ab09f229531f687ac002b24ba98700da6cf2e47e
andreshm99 <andres.hm99@gmail.com>
2021-10-18 00:14:45 +0200

    heroku

--- catalog/models.py
+++ catalog/models.py
@@ -33,6 +33,9 @@ class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
 
+    class Meta:
+            ordering = ['title']
+            
     # Foreign Key used because book can only have one author,
     # but authors can have multiple books
     # Author as a string rather than object because it hasn't
